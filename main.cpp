5758149db5304a35aa6c9af8bd7a6385405abcaa
David Möller <damoller99@hotmail.se>
2021-03-18 11:11:16 +0100

    Generating my own sound data

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 #include <vector>
 #include "wavUtil.h"
+#include <cmath>
+
+/**
+ * Returns sine wave of format AL_FORMAT_MONO8
+ */
+unsigned char* generateSine(std::int32_t sampleRate, ALsizei size, double frequency) {
+  auto data = new unsigned char[size];
+  for(int i = 0; i < size; i++) {
+    data[i] = 128 + 128 * sin(frequency * 2 * 3.141592 * ((double)i/sampleRate));
+  }
+  return data;
+}
 
 int main()
 {
@@ -32,16 +44,18 @@ int main()
     return 0;
   }
 
-  std::uint8_t channels;
-  std::int32_t sampleRate;
-  std::uint8_t bitsPerSample;
-  ALsizei size;
-  auto soundData = load_wav("/home/molle/Documents/projects/cppAudio/Alesis-Fusion-Acoustic-Bass-C2.wav", channels, sampleRate, bitsPerSample, size);
+  std::uint8_t channels = 1;
+  std::int32_t sampleRate = 44100;
+  std::uint8_t bitsPerSample = 8;
+  ALsizei size = 3 * sampleRate;
+  /*auto soundData = load_wav("/home/molle/Documents/projects/cppAudio/Alesis-Fusion-Acoustic-Bass-C2.wav", channels, sampleRate, bitsPerSample, size);
   if(!soundData)
   {
     std::cerr << "ERROR: Could not load wav" << std::endl;
     return 0;
-  }
+  }*/
+
+  auto soundData = generateSine(sampleRate, size, 880.0);
 
   ALuint buffer;
   alGenBuffers(1, &buffer);
